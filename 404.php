e88d9d2961a8fa0e403a64409f3acefc072769f3
Patrick <patrick.leisinger@gmail.com>
2019-05-10 17:19:43 +0200

    optimize search input fields and 404 page

--- 404.php
+++ 404.php
@@ -11,14 +11,7 @@ get_header(); ?>
     <div class="site-content">
 	    <div class="grid container">
 			<article>
-				<header class="page-header">
-					<h1 class="title-large"><?php esc_html_e( 'Not the Page you were looking for!', 'soapatricksix' ); ?></h1>
-				</header>
-				<hr>
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'soapatricksix' ); ?></p>
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-				</div>				
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 			</article>
 		</div>
 	</div>
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -10,15 +10,26 @@
 ?>
 
 <header class="page-header">
-	<h1 class="title-large"><?php esc_html_e( 'Nothing Found', 'soapatricksix' ); ?></h1>
+	<h1 class="title-large">
+		<?php 
+			if ( is_search() ) :
+				esc_html_e( 'Nothing Found', 'soapatricksix' );
+			else : 
+				esc_html_e( 'Not the Page you were looking for!', 'soapatricksix' );
+			endif;
+		?>
+	</h1>
 </header>
 <hr>
 <div class="page-content">
-	<?php if ( is_search() ) : ?>
-		<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'soapatricksix' ); ?></p>
-		<?php get_search_form(); ?>
-	<?php else : ?>
-		<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'soapatricksix' ); ?></p>
-		<?php get_search_form(); ?>
-	<?php endif; ?>
+	<?php 
+		if ( is_search() ) :
+			echo '<p>' . esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'soapatricksix' ) . '</p>';
+			
+		else :
+			echo '<p>' . esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'soapatricksix' ) . '</p>';		
+		endif;
+		get_search_form();
+		the_widget( 'WP_Widget_Recent_Posts' ); 
+	?>
 </div>

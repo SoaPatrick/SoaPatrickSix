384fa90e9707ed3187c535628094fdb8f8f6ace0
Patrick <patrick.leisinger@gmail.com>
2019-09-14 16:26:54 +0200

    change look and feel of navigation
    - add tooltip on hover
    - make nav full height
    - adjust everything accordingly
    - small changes to search (width and placeholder text)
    - change wording of search and not found

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -13,9 +13,9 @@
 	<h1 class="title-large">
 		<?php 
 			if ( is_search() ) :
-				esc_html_e( 'Nothing Found', 'soapatricksix' );
+				esc_html_e( 'I found nothing!', 'soapatricksix' );
 			else : 
-				esc_html_e( 'Not the Page you were looking for!', 'soapatricksix' );
+				esc_html_e( 'Something went wrong!', 'soapatricksix' );
 			endif;
 		?>
 	</h1>
@@ -24,10 +24,10 @@
 <div class="page-content">
 	<?php 
 		if ( is_search() ) :
-			echo '<p>' . esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'soapatricksix' ) . '</p>';
+			echo '<p>' . esc_html_e( 'Sorry, but I can&rsquo;t find what you&rsquo;re looking for. Please try again with other words.', 'soapatricksix' ) . '</p>';
 			
 		else :
-			echo '<p>' . esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'soapatricksix' ) . '</p>';		
+			echo '<p>' . esc_html_e( 'It seems I can&rsquo;t find what you&rsquo;re looking for. Try finding it?', 'soapatricksix' ) . '</p>';		
 		endif;
 		get_search_form();
 		the_widget( 'WP_Widget_Recent_Posts' ); 

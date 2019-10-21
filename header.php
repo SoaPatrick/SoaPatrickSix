ab6c4c090cad20e30ed97cb07e7694f77307a204
Patrick <patrick.leisinger@gmail.com>
2019-10-21 09:47:23 +0200

    minify inline script in head

--- header.php
+++ header.php
@@ -53,18 +53,7 @@
 	
 	-->	
 	<?php wp_head(); ?>
-	<script>
-		const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-		const currentColor = localStorage.getItem('color') ? localStorage.getItem('color') : null;
-		
-		if (currentTheme) {
-		    document.documentElement.setAttribute('data-theme', currentTheme);
-		}
-		
-		if (currentColor) {
-		    document.documentElement.setAttribute('data-color', currentColor);         
-		}
-	</script>
+	<script>const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;const currentColor = localStorage.getItem('color') ? localStorage.getItem('color') : null;if(currentTheme){document.documentElement.setAttribute('data-theme', currentTheme);}if(currentColor){document.documentElement.setAttribute('data-color', currentColor);}</script>
 	<script src="https://kit.fontawesome.com/89d3ae0c6b.js" crossorigin="anonymous"></script>
 </head>
 
